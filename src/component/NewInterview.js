d10fd38196bd787335976c524568e8d7c761d3bb
sissy5656 <supersissy5656@gmail.com>
2020-08-26 16:01:31 -0500

    keeping work

--- src/component/NewInterview.js
+++ src/component/NewInterview.js
@@ -1,14 +1,24 @@
 import React from "react";
-
+import "./Component.css";
 const NewInterview = () => {
   return (
-    <div>
-      <div className="big-picture">
+    <div className="first-section">
+      <div className="new-interview">
         <h1>New Interview</h1>
-        <img src="" alt="" />
-        <div className="interveiw-directory">
-          <h1>Invterview Directory</h1>
-          <div className="directory-grid"></div>
+        <img
+          className="new-interview-pic"
+          src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fthe-talks.com%2Fwp-content%2Fuploads%2F2014%2F09%2FKevin-Kline-01.jpg"
+          alt=""
+        />
+      </div>{" "}
+      <div className="interview-directory">
+        <h1>Invterview Directory</h1>
+        <div className="directory-grid">
+          <img
+            className="new-interview-pic"
+            src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fthe-talks.com%2Fwp-content%2Fuploads%2F2014%2F09%2FKevin-Kline-01.jpg"
+            alt=""
+          />
         </div>
       </div>
     </div>
--- src/navigations/Navbar.js
+++ src/navigations/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <h1>The Talks</h1>
       <h6>Interview Directory</h6>
       <h6>World Guide</h6>
